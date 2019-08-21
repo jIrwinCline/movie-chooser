bc9557a8aa8a59a50d2048250a9d55038c269d57
Student <example@example.com>
2019-08-21 15:31:22 -0700

    get app to send out proper searches for titles with spaces in name

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,9 +5,58 @@ include API
   def index
     response = API::Interface.call("/movies")
     @movies = JSON.parse(response)
+    render :index
+  end
+
+  def show
+    response = API::Interface.call("/movies/#{params[:id]}")
+    @movies = JSON.parse(response)
+    render :show
   end
 
   def random
     response = API::Interface.call("/random")
+    @movies = JSON.parse(response)
+    render :index
+  end
+
+  def longest
+    response = API::Interface.call("/longest")
+    @movies = JSON.parse(response)
+    render :index
+  end
+
+  def rating
+    response = API::Interface.call("/rating")
+    @movies = JSON.parse(response)
+    render :index
+  end
+
+  def popular
+    response = API::Interface.call("/popular")
+    @movies = JSON.parse(response)
+    render :index
+  end
+
+  def title_by_letter
+    letter = params[:letter]
+    response = API::Interface.call("/title_by_letter/#{letter}")
+    @movies = JSON.parse(response)
+    render :index
+  end
+
+  def title
+    title = params[:title]
+    title = title.gsub(" ", "%20")
+    response = API::Interface.call("/title/#{title}")
+    @movies = JSON.parse(response)
+    render :index
+  end
+
+  def year_released
+    year = params[:year]
+    response = API::Interface.call("/year_released/#{year}")
+    @movies = JSON.parse(response)
+    render :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/", to: "movies#index"
+  get '/movies/:id', to: "movies#show"
   get '/random', to: 'movies#random'
+  get '/longest', to: 'movies#longest'
+  get '/rating', to: 'movies#rating'
+  get '/popular', to: 'movies#popular'
+  get '/title_by_letter/:letter', to: 'movies#title_by_letter'
+  get '/title', to: 'movies#title'
+  get '/year_released/:year', to: 'movies#year_released'
 end
