356f6c281d0e3c9afa94f68c15f742d7e90824cc
Student <example@example.com>
2019-08-21 11:57:42 -0700

    add route for random. add view for index

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/", to: "movies#index"
-  get '/', to: 'movies#random'
+  get '/random', to: 'movies#random'
 end
