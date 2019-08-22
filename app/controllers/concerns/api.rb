9808f7158deb847647dd44ad9b596f73392f90ce
Student <example@example.com>
2019-08-22 09:21:15 -0700

    retool how we pass auth key through. now in headers and more secure.

--- app/controllers/concerns/api.rb
+++ app/controllers/concerns/api.rb
@@ -5,10 +5,15 @@ KEYS = Dotenv.load
 module API
   class Interface
     def self.call(url)
-      response = RestClient.get(
-        "localhost:3000/#{url}",
-        params: {token: KEYS["API_KEY"]}
+      response = RestClient::Request.execute(
+        :method => :get,
+        :url => "localhost:3000/#{url}",
+        :headers => {AUTHORIZATION: KEYS["API_KEY"]}
       )
+  #     response = RestClient.post(
+  # "localhost:3000/#{url}",
+  # request,
+  # :headers => :json, :accept => :json, :'x-auth-key' => "mykey")
     end
   end
 end
